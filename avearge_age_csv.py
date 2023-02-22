4a3b9b37c235139e66b4acd46fedb3be94ae8ad8
Moses-Morris <mosesmorrisdev@gmail.com>
2023-02-22 13:04:54 +0300

    Compute The total age and the sum, get the number of ages in the file, and calculate the avearge of the ages.

--- avearge_age_csv.py
+++ avearge_age_csv.py
@@ -9,7 +9,7 @@ data_from_file = list(read_File)
 #print(len(data_from_file))
 
 length = len(data_from_file)
-sum = 0
+total = 0
 for eachRow in data_from_file[1:]:
     age = eachRow[4]
     try:
@@ -18,8 +18,9 @@ for eachRow in data_from_file[1:]:
         print("Error. Invalid age Type: ", age)
 
     print(age)
-    sum  += sum + age
+    total  +=  age
 
-average = sum / length
+print("The total age is : ", total)
+average = total / length
 print("The average age is : ", average)
 open_File.close()
