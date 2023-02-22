f679bdbfb4e66de9168215c887ec2a45743f8c9f
Moses-Morris <mosesmorrisdev@gmail.com>
2023-02-22 12:59:04 +0300

    Fix : open and read data in a csv file.

---
+++ avearge_age_csv.py
@@ -0,0 +1,25 @@
+from csv import reader
+
+open_File = open("titanic.csv")
+read_File = reader(open_File)
+
+data_from_file = list(read_File)
+
+#print(data_from_file[0:])
+#print(len(data_from_file))
+
+length = len(data_from_file)
+sum = 0
+for eachRow in data_from_file[1:]:
+    age = float(eachRow[4])
+    #try:
+        #age = int(age)
+    #except:
+        #print("Error. Invalid age Type: ", age)
+
+    print(age)
+    sum  += sum + age
+
+average = sum / length
+print("The average age is : ", average)
+open_File.close()
--- open_csv.py
+++ open_csv.py
@@ -5,21 +5,7 @@ read_File = reader(open_File)
 
 data_from_file = list(read_File)
 
-#print(data_from_file[0:])
-#print(len(data_from_file))
+print(data_from_file[0:])
+print("Number of Rows : ",len(data_from_file))
 
-length = len(data_from_file)
-sum = 0
-for eachRow in data_from_file[1:]:
-    age = float(eachRow[4])
-    #try:
-        #age = int(age)
-    #except:
-        #print("Error. Invalid age Type: ", age)
-
-    print(age)
-    sum  += sum + age
-
-average = sum / length
-print("The average age is : ", average)
 open_File.close()
